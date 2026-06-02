c1b0070c15066064caab3150606dca2d3540de88
Abhishek Maurya <abhishekmaurya8237@gmail.com>
2026-06-02 08:38:57 +0530

    Update question_no_05.java

--- Day_2/question_no_05.java
+++ Day_2/question_no_05.java
@@ -9,7 +9,7 @@ public class question_no_05 {
         int n = sc.nextInt();
         int rem, sum = 0;
 
-        while(n > 0){
+        while(n != 0){
             rem = n % 10;
             sum += rem;
             n /= 10;
